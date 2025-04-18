b599367db2486c8105a282ccae870331b5cc1821
Maheen Sohail <maheensohail2003@gmail.com>
2025-04-18 22:33:51 +0500

    Initial commit from Create Next App

---
+++ components/Footer.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { MagicButton } from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
+import { socialMedia } from '@/data'
+
+const Footer = () => {
+  return (
+    <footer className='w-full mb-[100px] md:mb-5 pb-10' id="contact">
+        <div className='flex flex-col items-center md:text-2xl lg:text-3xl text-xl'>
+            <h1 className='lg:max-w-[50vw]'>
+                Ready to take <span className='text-purple'>your</span> digital presence to the next level?
+            </h1>
+            <p className='text-white-200 md:mt-10 my-5 text-center'> Reach out to me today and let&apos;s discuss how we can achieve your goals.</p>
+            <a href='mailto: sohailmaheen6@gmial.com'>
+                <MagicButton
+                title="Let's get in touch"
+                icon={<FaLocationArrow />}
+                position='right' />
+            </a>
+        </div>
+
+        <div className='flex mt-16 md:flex-row flex-col justify-between items-center'>
+            <p className='md:text-base text-sm md:font-normal font-light'>
+                Copyright © 2025 Maheen
+            </p>
+
+            <div className='flex items-center md:gap-3 gap-6 '>
+                {socialMedia.map((profile)=> (
+                    <div key={profile.id} className='flex w-10 h-10 cursor-pointer justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300' > 
+                        <img src={profile.img}
+                        alt='' width={20} height={20}
+                         />
+                    </div>
+                ))}
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
