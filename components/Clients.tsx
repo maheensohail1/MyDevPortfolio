0d0d6e84d282dd3847eb479ecdf6daa43004c210
Maheen Sohail <maheensohail2003@gmail.com>
2025-04-22 14:36:39 +0500

    lyra added to projects

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -22,6 +22,7 @@ const Clients = () => {
                         </div>
                     ))}
                 </div>
+                
             </div>
         </div>
     )
--- data/index.ts
+++ data/index.ts
@@ -74,11 +74,11 @@ export const navItems = [
   export const projects = [
     {
       id: 1,
-      title: "3D Solar System Planets to Explore",
-      des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-      img: "/p1.svg",
-      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-      link: "https://github.com/adrianhajdin?tab=repositories",
+      title: "LYRA - Powered by Gemini",
+      des: "Reimagining of Gemini platform, focused on delivering similar functionality with improved design",
+      img: "/lyra-poster.png",
+      iconLists: ["/re.svg", "/js.png", "/vite.png", "/gemini.png"],
+      link: "https://lyra-gemini-ai.vercel.app/",
     },
     {
       id: 2,
@@ -88,22 +88,7 @@ export const navItems = [
       iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
       link: "https://github.com/adrianhajdin/zoom-clone",
     },
-    {
-      id: 3,
-      title: "AI Image SaaS - Canva Application",
-      des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-      img: "/p3.svg",
-      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-      link: "https://github.com/adrianhajdin/ai_saas_app",
-    },
-    {
-      id: 4,
-      title: "Animated Apple Iphone 3D Website",
-      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-      img: "/p4.svg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-      link: "https://github.com/adrianhajdin/iphone",
-    },
+    
   ];
   
   export const testimonials = [
